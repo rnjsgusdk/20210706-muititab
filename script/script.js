a97d23f509b84b297338481933bd1d9c05f1d3cb
rnjsgusdk <rnjsgusdk>
2021-07-07 00:18:38 +0900

    20210706muititab

---
+++ script/script.js
@@ -0,0 +1,32 @@
+/*eslint-disable*/
+
+$(function(){
+
+
+
+    $("botton.tabbtn").click(function(){
+        $("botton.tabbtn").css({
+            "border": '1px solid #aaa',
+            "background-color":'#eee'
+        });
+
+        $(this).css({
+            "border-bottm-color": "#fff",
+            "background-color": "#fff"
+        })
+    });
+
+
+
+    /*내용css */
+    $("div,tabcontents").css({"dispaly":"none"});
+
+    var $(this).attr("data-link");
+
+
+
+    $("#" + datelink).css({"dispaly":"bolck"});
+
+     /*$("#galleryarema").css({"dispaly":"bolck"});*/
+
+});
